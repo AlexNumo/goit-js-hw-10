281d7eb719c72f55520119bbe6b70e80fb2de479
Oleksandr Arseniuk <korol1511@gmail.com>
2021-11-25 20:23:17 +0200

    HW 10 done

--- src/index.js
+++ src/index.js
@@ -1,50 +1,69 @@
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './fetchCountries';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
 
-const inputCountry = document.querySelector("#search-box");
-const countryList = document.querySelector(".country-list");
-const countryInfo = document.querySelector(".country-info");
+const cardContainer = document.querySelector('.country-info');
+const listContainer = document.querySelector('.country-list');
+const inputEl = document.querySelector('input#search-box');
 
-inputCountry.addEventListener("click", () => {
-  fetchCountry()
-    .then((name) => renderCountryName(name))
-    .catch((error) => console.log(error));
-});
+inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
-function fetchCountry() {
-    return fetch("https://restcountries.com/v3.1/")
-        .then((response) => {
-      if (!response.ok) {
-        throw new Error(response.status);
+function onInput() {
+  const searchQuery = inputEl.value.trim();
+  fetchCountries(searchQuery)
+    .then(data => {
+      clearContainers();
+
+      if (data.length === 1) {
+        resetInput();
+        renderCountryCard(data);
+        return;
+      } else if (data.length > 1 && data.length <= 10) {
+        renderCountriesList(data);
+        return;
       }
-      return response.json();
-    }
-  );
-}
-
-function renderCountryName(name) {
-  const markup = name
-    .map((name) => {
-      return `<li>
-          <p><b>Name</b>: ${name.official}</p>
-          <p><b>Capital</b>: ${name.capital}</p>
-          <p><b>Population</b>: ${name.population}</p>
-          <p><b>Flags</b>: ${name.flags.svg}</p>
-          <p><b>Languages</b>: ${name.languages}</p>
-        </li>`;
+      Notify.info('Too many matches found. Please enter a more specific name.');
     })
-    .join("");
-  countryList.innerHTML = markup;
+    .catch(onFetchError);
 }
 
-fetch("https://restcountries.com/v3.1/name/USA")
-  .then(response => {
-    // Response handling
-  })
-  .then(data => {
-    // Data handling
-  })
-  .catch(error => {
-    // Error handling
+function renderCountryCard([{ flags, name, capital, population, languages }]) {
+  cardContainer.innerHTML = `<div class="country-symbolics">
+  <img class="country-flag"src="${flags.svg}" alt="country flag" width="50" />
+  <h1 class="country-name">${name.official}</h1>
+  </div>
+        <ul class="country-summary-list">
+          <li class="country-summary-item"><b>Capital:</b> ${capital}</li>
+          <li class="country-summary-item"><b>Population:</b> ${population}</li>
+          <li class="country-summary-item"><b>Languages:</b> ${Object.values(languages).join(
+            ', ',
+          )}</li>
+        </ul>`;
+}
+
+function renderCountriesList(countries) {
+  countries.forEach(({ flags, name }) => {
+    const markup = `<li class="country">
+      <img class="country-flag"src="${flags.svg}" alt="country flag" width="40" />
+  <p class="country-name">${name.common}</p>
+  </li>`;
+    listContainer.insertAdjacentHTML('beforeend', markup);
   });
+}
+
+function onFetchError(error) {
+  Notify.failure(`Oops, there is no country with that name`);
+  resetInput();
+}
+
+function resetInput() {
+  inputEl.value = null;
+}
+
+function clearContainers() {
+  cardContainer.innerHTML = '';
+  listContainer.innerHTML = '';
+}
